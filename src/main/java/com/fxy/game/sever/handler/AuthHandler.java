4031b7fc510bfa524534e6332c4cb1d9497ec402
fengxueyoung <1070606270@qq.com>
2025-06-25 16:56:45 +0800

    202506251656

--- src/main/java/com/fxy/game/sever/handler/AuthHandler.java
+++ src/main/java/com/fxy/game/sever/handler/AuthHandler.java
@@ -1,10 +1,7 @@
 package com.fxy.game.sever.handler;
 
-import com.fxy.game.core.DataStore;
-import com.fxy.game.core.SessionManager;
-import com.fxy.game.message.AuthRequest;
-import com.fxy.game.message.AuthResponse;
 import com.fxy.game.message.Wrapper;
+import com.fxy.game.sever.service.AuthService;
 import com.fxy.game.util.ServerUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -27,64 +24,13 @@ public class AuthHandler extends SimpleChannelInboundHandler<Wrapper> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Wrapper msg) {
-        Wrapper.MsgCase msgCase = msg.getMsgCase();
-        if (msgCase == Wrapper.MsgCase.AUTHREQUEST) {
-            AuthRequest request = msg.getAuthRequest();
-            switch (request.getType()) {
-                case REGISTER -> {
-                    handlerRegister(ctx, request);
-                }
-                case LOGIN -> {
-                    handlerLogin(ctx, request);
-                }
-                default -> {
-                    sendAuthFail(ctx, "未知操作!");
-                }
-            }
-        } else {
-            sendAuthFail(ctx, "请先登录!");
+        Wrapper wrapper = AuthService.getInstance().handleAuth(ctx, msg);
+        ctx.writeAndFlush(wrapper);
+        if (wrapper.getAuthResponse().getSuccess()) {
+            ctx.pipeline().remove(this);
         }
     }
 
-    private void handlerRegister(ChannelHandlerContext ctx, AuthRequest request) {
-        if (!DataStore.exists(request.getUsername())) {
-            DataStore.create(request.getUsername(), request.getPassword());
-            sendAuthSuccess(ctx, request.getUsername());
-        } else {
-            sendAuthFail(ctx, "用户名已存在!");
-        }
-    }
-
-    private void handlerLogin(ChannelHandlerContext ctx, AuthRequest request) {
-        if (!DataStore.exists(request.getUsername())) {
-            sendAuthFail(ctx, "用户不存在!");
-        } else {
-            if (DataStore.verify(request.getUsername(), request.getPassword())) {
-                sendAuthSuccess(ctx, request.getUsername());
-            } else {
-                sendAuthFail(ctx, "用户名或密码错误!");
-            }
-        }
-
-    }
-
-    private void sendAuthFail(ChannelHandlerContext ctx, String msg) {
-        ctx.writeAndFlush(Wrapper.newBuilder()
-                .setAuthResponse(AuthResponse.newBuilder()
-                        .setSuccess(false)
-                        .setMessage(msg)
-                        .build()).build());
-    }
-
-    private void sendAuthSuccess(ChannelHandlerContext ctx, String username) {
-        ctx.writeAndFlush(Wrapper.newBuilder()
-                .setAuthResponse(AuthResponse.newBuilder()
-                        .setSessionId(SessionManager.createSession(username, ctx.channel()))
-                        .setSuccess(true)
-                        .setMessage("登录成功!")
-                        .build()).build());
-        ctx.pipeline().remove(this);
-    }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
---
+++ src/main/java/com/fxy/game/sever/service/AuthService.java
@@ -0,0 +1,93 @@
+package com.fxy.game.sever.service;
+
+import com.fxy.game.core.DataStore;
+import com.fxy.game.core.SessionManager;
+import com.fxy.game.message.AuthRequest;
+import com.fxy.game.message.AuthResponse;
+import com.fxy.game.message.Wrapper;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author FengXueyang
+ * @Title AuthService
+ * @Date 2025/6/25 16:37
+ **/
+public class AuthService {
+
+    private static volatile AuthService authService = null;
+
+    public static AuthService getInstance() {
+        if (authService == null) {
+            synchronized (AuthService.class) {
+                if (authService == null) {
+                    authService = new AuthService();
+                    return authService;
+                } else {
+                    return authService;
+                }
+            }
+        } else {
+            return authService;
+        }
+    }
+
+    public Wrapper handleAuth(ChannelHandlerContext ctx, Wrapper wrapper) {
+        Wrapper.MsgCase msgCase = wrapper.getMsgCase();
+        if (msgCase == Wrapper.MsgCase.AUTHREQUEST) {
+            AuthRequest request = wrapper.getAuthRequest();
+            switch (request.getType()) {
+                case REGISTER -> {
+                    return handlerRegister(ctx, request);
+                }
+                case LOGIN -> {
+                    return handlerLogin(ctx, request);
+                }
+                default -> {
+                    return sendAuthFail("未知操作!");
+                }
+            }
+        } else {
+            return sendAuthFail("请先登录!");
+        }
+    }
+
+
+    private Wrapper handlerRegister(ChannelHandlerContext ctx, AuthRequest request) {
+        if (!DataStore.exists(request.getUsername())) {
+            DataStore.create(request.getUsername(), request.getPassword());
+            return sendAuthSuccess(ctx, request.getUsername());
+        } else {
+            return sendAuthFail("用户名已存在!");
+        }
+    }
+
+    private Wrapper handlerLogin(ChannelHandlerContext ctx, AuthRequest request) {
+        if (!DataStore.exists(request.getUsername())) {
+            return sendAuthFail("用户不存在!");
+        } else {
+            if (DataStore.verify(request.getUsername(), request.getPassword())) {
+                return sendAuthSuccess(ctx, request.getUsername());
+            } else {
+                return sendAuthFail("用户名或密码错误!");
+            }
+        }
+
+    }
+
+    private Wrapper sendAuthFail(String msg) {
+        return Wrapper.newBuilder()
+                .setAuthResponse(AuthResponse.newBuilder()
+                        .setSuccess(false)
+                        .setMessage(msg)
+                        .build()).build();
+    }
+
+    private Wrapper sendAuthSuccess(ChannelHandlerContext ctx, String username) {
+        return Wrapper.newBuilder()
+                .setAuthResponse(AuthResponse.newBuilder()
+                        .setSessionId(SessionManager.createSession(username, ctx.channel()))
+                        .setSuccess(true)
+                        .setMessage("登录成功!")
+                        .build()).build();
+    }
+}
